5c3e520d9e7031dd3248d2477f0dab9daa504b1e
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-10-20 00:20:50 -0400

    Complete all aspects of Gittip's Tips API.

--- lib/gratitude/tips.rb
+++ lib/gratitude/tips.rb
@@ -2,16 +2,53 @@ module Gratitude
   class Client
     module Tips
 
+      def current_tips
+        self.class.get(tips_url, { :basic_auth => authorization }).parsed_response
+      end
+
+      def current_tips_total
+        current_tips.inject(0) { |total, tip| total + tip["amount"].to_f }
+      end
+
+      def update_tips(array_of_hashes_with_usernames_and_amounts)
+        self.class.post(tips_url,
+        {
+          :body => prepared_tips_array(array_of_hashes_with_usernames_and_amounts).to_json,
+          :basic_auth => authorization,
+          :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+          })
+      end
+
+      def update_tips_and_prune(array_of_hashes_with_usernames_and_amounts)
+        self.class.post(tips_url,
+        {
+          :body => prepared_tips_array(array_of_hashes_with_usernames_and_amounts).to_json,
+          :basic_auth => authorization,
+          :query => { :also_prune => "true"},
+          :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+          })
+      end
+
       def tips_url
         "https://www.gittip.com/#{username}/tips.json"
       end
 
       def authorization
-        { :basic_auth => { :username => api_key } }
+        { :username => api_key }
       end
 
-      def current_tips
-        self.class.get(tips_url, authorization).parsed_response
+      def tip_hash_based_upon(username, amount)
+        { "amount" => "#{amount}", "platform" => "gittip", "username" => "#{username}" }
+      end
+
+      def prepared_tips_array(array_of_hashes_with_usernames_and_amounts)
+        prepared_array = []
+        array_of_hashes_with_usernames_and_amounts.each do |hash|
+          username = hash[:username] || hash["username"]
+          amount = hash[:amount] || hash["amount"]
+          prepared_array << tip_hash_based_upon(username, amount)
+        end
+        prepared_array
       end
 
     end # Tips
--- spec/gratitude/tips_spec.rb
+++ spec/gratitude/tips_spec.rb
@@ -7,47 +7,181 @@ describe Gratitude::Client::Tips do
 
   let(:client) { Gratitude::Client.new(:username => username, :api_key => api_key) }
 
-  describe "#tips_url" do
-    it "returns the correct tips_url" do
-      expect(client.tips_url).to eq("https://www.gittip.com/gratitude_test/tips.json")
-    end
-  end
+  describe "methods that do not interact with the API" do
 
-  describe "#authorization" do
-    it "returns the correct authorization hash" do
-      expect(client.authorization).to eq({ :basic_auth => { :username =>  api_key } })
+    describe "#tips_url" do
+      it "returns the correct tips_url" do
+        expect(client.tips_url).to eq("https://www.gittip.com/gratitude_test/tips.json")
+      end
     end
-  end
+
+    describe "#authorization" do
+      it "returns the correct authorization hash" do
+        expect(client.authorization).to eq({ :username =>  api_key })
+      end
+    end
+
+    describe "#tip_hash_based_upon(username, amount)" do
+      it "returns the correct tip hash when given an integer" do
+        expect(client.tip_hash_based_upon("whit537", 1)).to eq(
+          { "amount" => "1", "platform" => "gittip", "username" => "whit537" })
+      end
+
+      it "returns the correct tip hash when given an integer in a string" do
+        expect(client.tip_hash_based_upon("whit537", "1")).to eq(
+          { "amount" => "1", "platform" => "gittip", "username" => "whit537" })
+      end
+
+      it "returns the correct tip hash when given a float" do
+        expect(client.tip_hash_based_upon("whit537", 0.25)).to eq(
+          { "amount" => "0.25", "platform" => "gittip", "username" => "whit537" })
+      end
+
+      it "returns the correct tip hash when given a float" do
+        expect(client.tip_hash_based_upon("whit537", "0.25")).to eq(
+          { "amount" => "0.25", "platform" => "gittip", "username" => "whit537" })
+      end
+    end #single_tip_hash
+
+
+    describe "#tips_array" do
+
+      context "when using symbols as keys" do
+        it "returns the correct tips array when given an array with one tip" do
+          expect(client.prepared_tips_array([:username => "whit537", :amount => "1"])).to eq(
+            [{ "amount" => "1", "platform" => "gittip", "username" => "whit537" }])
+        end
+
+        it "returns the correct tips array when given an array with two tips" do
+          expect(client.prepared_tips_array([
+            { :username => "whit537", :amount => "1" },
+            { :username => "JohnKellyFerguson", :amount => "0.25" }
+          ])).to eq([
+            { "amount" => "1", "platform" => "gittip", "username" => "whit537" },
+            { "amount" => "0.25", "platform" => "gittip", "username" => "JohnKellyFerguson" }
+            ])
+        end
+      end # symbols as keys
+
+      context "when using strings as keys" do
+        it "returns the correct tips array when given an array with one tip" do
+          expect(client.prepared_tips_array(["username" => "whit537", "amount" => "1"])).to eq(
+            [{ "amount" => "1", "platform" => "gittip", "username" => "whit537" }])
+        end
+
+        it "returns the correct tips array when given an array with one tip" do
+              expect(client.prepared_tips_array([
+                { "username" => "whit537", "amount" => "1" },
+                { "username" => "JohnKellyFerguson", "amount" => "0.25" }
+              ])).to eq([
+                { "amount" => "1", "platform" => "gittip", "username" => "whit537" },
+                { "amount" => "0.25", "platform" => "gittip", "username" => "JohnKellyFerguson" }
+                ])
+        end
+      end # strings as keys
+
+    end #tips_array
+
+  end # methods that do not interact with the API
 
 
   describe "authentication and api requests" do
 
-    let(:current_tips) { [{"amount"=>"1.00", "platform"=>"gittip", "username"=>"whit537"},
-                          {"amount"=>"0.25", "platform"=>"gittip", "username"=>"JohnKellyFerguson"}] }
+    context "GET Requests" do
+      let(:current_tips) { [{"amount"=>"1.00", "platform"=>"gittip", "username"=>"whit537"},
+                            {"amount"=>"0.25", "platform"=>"gittip", "username"=>"JohnKellyFerguson"}] }
 
-    before do
-      VCR.insert_cassette "tips"
-    end
-
-    after do
-      VCR.eject_cassette
-    end
-
-    describe "#current_tips" do
-      it "returns the correct array of current tips" do
-        expect(client.current_tips).to eq (current_tips)
+      before do
+        VCR.insert_cassette "get_tips"
       end
-    end
 
-    describe "#update_tip" do
-    end
+      after do
+        VCR.eject_cassette
+      end
 
-    describe "#update_tips" do
-    end
+      describe "#current_tips" do
+        it "returns the correct array of current tips" do
+          expect(client.current_tips).to eq (current_tips)
+        end
+      end
 
-    describe "#update_tips_and_prune" do
-    end
+      describe "#current_tips_total" do
+        it "returns the correct total of current tips" do
+          expect(client.current_tips_total).to eq(1.25)
+        end
+      end
+    end #GET Requests
 
+    context "POST Requests" do
+
+      describe "#update_tips" do
+
+        context "when updating a single tip" do
+          let(:single_tip_response) { [{"amount"=>"5", "platform"=>"gittip", "username"=>"whit537"}] }
+
+          before do
+            VCR.insert_cassette "post_single_tip"
+          end
+
+          after do
+            VCR.eject_cassette
+          end
+
+          it "updates the correct tip information" do
+            expect(
+              client.update_tips(
+                [{ :username => "whit537", :amount => "5" }]
+                ).parsed_response).to eq(single_tip_response)
+          end
+        end #updating a single tip
+
+        context "when updating multiple tips" do
+          let(:multi_tip_response) { [
+                                      {"amount"=>"10", "platform"=>"gittip", "username"=>"whit537"},
+                                      {"amount"=>"4", "platform"=>"gittip", "username"=>"JohnKellyFerguson"}
+                                      ] }
+
+          before do
+            VCR.insert_cassette "post_multiple_tips"
+          end
+
+          after do
+            VCR.eject_cassette
+          end
+
+          it "updates the correct tip information" do
+            expect(
+              client.update_tips(
+                [{ :username => "whit537", :amount => "10" },
+                 { :username => "JohnKellyFerguson", :amount => "4"}]
+                ).parsed_response).to eq(multi_tip_response)
+          end
+        end # updating multiple tips
+
+        # context "when passing in a non-existent username" do
+        #   before do
+        #     VCR.insert_cassette "post_tip_error"
+        #   end
+
+        #   after do
+        #     VCR.eject_cassette
+        #   end
+
+        #   it "raises an error" do
+        #     expect(
+        #       client.update_tips(
+        #         [ {:username => "not_a_real_user", :amount => "20"} ]
+        #         ).parsed_response).to eq()
+        #   end
+        # end # non-existent username
+
+      end #update_tips
+
+      describe "#update_tips_and_prune" do
+
+      end
+
+    end #POST Requests
 
   end # authentication and api requests
 end
