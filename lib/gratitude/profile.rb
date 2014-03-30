56e9c42ed3fa2152691988a4ac05b9bfca4bf7c9
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-03-29 21:20:42 -0400

    Update Profile with additional data from api

--- lib/gratitude/profile.rb
+++ lib/gratitude/profile.rb
@@ -12,46 +12,56 @@ module Gratitude
     end
     alias_method :avatar, :avatar_url
 
-    def bitbucket_api_url
+    def bitcoin
+      response_body["bitcoin"]
+    end
+
+    def bitbucket
       response_body["elsewhere"]["bitbucket"]
     end
-    alias_method :bitbucket, :bitbucket_api_url
 
     def bitbucket_username
-      if bitbucket_api_url
-        bitbucket_api_url.gsub("https://bitbucket.org/api/1.0/users/", "")
-      end
+      bitbucket["user_name"] if bitbucket
     end
 
-    def bountysource_api_url
+    def bountysource
       response_body["elsewhere"]["bountysource"]
     end
-    alias_method :bountysource, :bountysource_api_url
 
     def bountysource_username
-      if bountysource_api_url
-        bountysource_api_url.gsub("https://api.bountysource.com/users/", "")
-      end
+      bountysource["user_name"] if bountysource
     end
 
-    def github_api_url
-      response_body["elsewhere"]["github"]
+    def github
+      response_body["elsewhere"]["bountysource"]
     end
-    alias_method :github, :github_api_url
 
     def github_username
-      if github_api_url
-        github_api_url.gsub("https://api.github.com/users/", "")
-      end
+      github["user_name"] if github
     end
 
-    def twitter_api_url
+    def openstreetmap
+      response_body["elsewhere"]["openstreetmap"]
+    end
+
+    def openstreetmap_username
+      openstreetmap["user_name"] if openstreetmap
+    end
+
+    def twitter
       response_body["elsewhere"]["twitter"]
     end
-    alias_method :twitter, :twitter_api_url
 
     def twitter_username
-      nil
+      twitter["user_name"] if twitter
+    end
+
+    def venmo
+      response_body["elsewhere"]["venmo"]
+    end
+
+    def venmo_username
+      venmo["user_name"] if venmo
     end
 
     def amount_giving
@@ -77,6 +87,15 @@ module Gratitude
       response_body["id"]
     end
 
+    def number_of_patrons
+      response_body["npatrons"]
+    end
+    alias_method :npatrons, :number_of_patrons
+
+    def on
+      response_body["on"]
+    end
+
     private
 
     def response
@@ -86,5 +105,6 @@ module Gratitude
     def response_body
       @response_body ||= response.body
     end
+
   end # Profile
 end # Gratitude
--- spec/gratitude/profile_spec.rb
+++ spec/gratitude/profile_spec.rb
@@ -9,210 +9,290 @@ describe Gratitude::Profile do
   end
 
   describe "instance methods" do
+    before { VCR.insert_cassette "profiles" }
+    after { VCR.eject_cassette }
+    let(:complete_profile) { Gratitude::Profile.new("JohnKellyFerguson") }
+    let(:incomplete_profile) { Gratitude::Profile.new("gratitude_test") }
+    let(:no_twitter_profile) { Gratitude::Profile.new("goya") }
 
-    context "a fully completed profile" do
-      before { VCR.insert_cassette "complete_profile" }
-      after { VCR.eject_cassette }
-      let(:complete_profile) { Gratitude::Profile.new("whit537") }
+    it "sets the correct username" do
+      expect(complete_profile.username).to eq("JohnKellyFerguson")
+    end
 
-      it "sets the correct username" do
-        expect(complete_profile.username).to eq("whit537")
+    describe "json response" do
+      it "returns the correct keys in the json hash" do
+        expect(complete_profile.send(:response_body).keys)
+          .to eq(
+            [
+              "avatar",
+              "bitcoin",
+              "elsewhere",
+              "giving",
+              "goal",
+              "id",
+              "npatrons",
+              "number",
+              "on",
+              "receiving",
+              "username"
+              ]
+            )
+      end
+    end
+
+    describe "#avatar_url" do
+      it "returns a string" do
+        expect(complete_profile.avatar_url.class).to be(String)
       end
 
-      describe "#avatar_url" do
-        it "returns the correct avatar url" do
-          expect(complete_profile.avatar_url)
-            .to eq("https://www.gravatar.com/avatar/fb054b407a6461e417ee6b6ae084da37.jpg?s=128")
-        end
-
-        it "returns the same value as its alias: #avatar" do
-          expect(complete_profile.avatar_url).to eq(complete_profile.avatar)
-        end
+      it "returns the same value as its alias: #avatar" do
+        expect(complete_profile.avatar_url).to eq(complete_profile.avatar)
       end
+    end
 
-      describe "#bitbucket_api_url" do
-        it "returns the correct bitbucket api url" do
-          expect(complete_profile.bitbucket_api_url)
-            .to eq("https://bitbucket.org/api/1.0/users/whit537")
-        end
-
-        it "returns the same value as its alias: #bitbucket" do
-          expect(complete_profile.bitbucket_api_url).to eq(complete_profile.bitbucket)
-        end
-      end
-
-      describe "#bitbucket_username" do
-        it "returns the correct bitbucket username" do
-          expect(complete_profile.bitbucket_username).to eq("whit537")
-        end
-      end
-
-      describe "#bountysource_api_url" do
-        it "returns the correct bountysource api url" do
-          expect(complete_profile.bountysource_api_url)
-            .to eq("https://api.bountysource.com/users/whit537")
-        end
-      end
-
-      describe "#bountysource_username" do
-        it "returns the correct bountysource username" do
-          expect(complete_profile.bountysource_username).to eq("whit537")
+    describe "#bitcoin" do
+      context "a user with an associated bitcoin address" do
+        it "returns a string" do
+          expect(complete_profile.bitcoin.class).to be(String)
         end
       end
 
-      describe "#github_api_url" do
-        it "returns the correct github api url" do
-          expect(complete_profile.github_api_url)
-            .to eq("https://api.github.com/users/whit537")
+      context "a user without an associated bitcoin address" do
+        it "returns a string" do
+          expect(incomplete_profile.bitcoin).to be(nil)
         end
+      end
+    end
 
-        it "returns the same value as its alias: #github" do
-          expect(complete_profile.github_api_url).to eq(complete_profile.github)
+    describe "#bitbucket" do
+      context "a user with an associated bitbucket account" do
+        it "returns a hash" do
+          expect(complete_profile.bitbucket.class).to be(Hash)
         end
       end
 
-      describe "#github_username" do
-        it "returns the correct github username" do
-          expect(complete_profile.github_username).to eq("whit537")
+      context "a user without an associated bitbucket acount" do
+        it "returns a string" do
+          expect(incomplete_profile.bitbucket).to be(nil)
+        end
+      end
+    end
+
+    describe "#bitbucket_username" do
+      context "a user with an associated bitbucket account" do
+        it "returns a string" do
+          expect(complete_profile.bitbucket_username.class).to be(String)
         end
       end
 
-      describe "#twitter_api_url" do
-        it "returns the correct twitter api url" do
-          expect(complete_profile.twitter_api_url)
-            .to eq("https://api.twitter.com/1.1/users/show.json?id=34175404&include_entities=1")
-        end
-
-        it "returns the same value as its alias: #twitter" do
-          expect(complete_profile.twitter_api_url).to eq(complete_profile.twitter)
-        end
-      end
-
-      describe "#twitter_username" do
-        it "returns nil" do
-          expect(complete_profile.twitter_username).to be(nil)
-        end
-      end
-
-      describe "#amount_giving" do
-        it "returns a float" do
-          expect(complete_profile.amount_giving.class).to be(Float)
-        end
-
-        it "returns the correct amount giving" do
-          expect(complete_profile.amount_giving).to eq(101.41)
-        end
-
-        it "returns the same value as its alias: #giving" do
-          expect(complete_profile.amount_giving).to eq(complete_profile.giving)
-        end
-      end
-
-      describe "#amount_receiving" do
-        it "returns a float" do
-          expect(complete_profile.amount_receiving.class).to be(Float)
-        end
-
-        it "returns the correct amount amount receiving" do
-          expect(complete_profile.amount_receiving).to eq(434.25)
-        end
-
-        it "returns the same value as its alias: #receiving" do
-          expect(complete_profile.amount_receiving)
-            .to eq(complete_profile.receiving)
-        end
-      end
-
-      describe "#goal" do
-        it "returns the correct goal" do
-          expect(complete_profile.goal).to be(nil)
-        end
-      end
-
-      describe "#account_type" do
-        it "returns the correct account_type" do
-          expect(complete_profile.account_type).to eq("singular")
-        end
-
-        it "returns the same value as its alias: #number" do
-          expect(complete_profile.account_type).to eq(complete_profile.number)
-        end
-      end
-
-      describe "#id" do
-        it "returns a fixnum" do
-          expect(complete_profile.id.class).to be(Fixnum)
-        end
-
-        it "returns the correct id number" do
-          expect(complete_profile.id).to eq(1451)
-        end
-      end
-
-    end # a fully completed profile
-
-    context "an account with only twitter as a linked account" do
-      before { VCR.insert_cassette "incomplete_profile" }
-      after { VCR.eject_cassette }
-      let(:incomplete_profile) { Gratitude::Profile.new("gratitude_test") }
-
-      describe "#bitbucket_api_url" do
-        it "returns nil" do
-          expect(incomplete_profile.bitbucket_api_url).to be(nil)
-        end
-      end
-
-      describe "#bitbucket_username" do
-        it "returns nil" do
+      context "a user without an associated bitbucket acount" do
+        it "returns a string" do
           expect(incomplete_profile.bitbucket_username).to be(nil)
         end
       end
+    end
 
-      describe "#bountysource_api_url" do
-        it "returns nil" do
-          expect(incomplete_profile.bountysource_api_url).to be(nil)
+    describe "#bountysource" do
+      context "a user with an associated bountysource account" do
+        it "returns a hash" do
+          expect(complete_profile.bountysource.class).to be(Hash)
         end
       end
 
-      describe "#bountysource_username" do
-        it "returns nil" do
+      context "a user without an associated bountysource acount" do
+        it "returns a string" do
+          expect(incomplete_profile.bountysource).to be(nil)
+        end
+      end
+    end
+
+    describe "#bountysource_username" do
+      context "a user with an associated bountysource account" do
+        it "returns a string" do
+          expect(complete_profile.bountysource_username.class).to be(String)
+        end
+      end
+
+      context "a user without an associated bountysource acount" do
+        it "returns a string" do
           expect(incomplete_profile.bountysource_username).to be(nil)
         end
       end
+    end
 
-      describe "#github_api_url" do
-        it "returns nil" do
-          expect(incomplete_profile.github_api_url).to be(nil)
+    describe "#github" do
+      context "a user with an associated github account" do
+        it "returns a hash" do
+          expect(complete_profile.github.class).to be(Hash)
         end
       end
 
-      describe "#github_username" do
-        it "returns nil" do
+      context "a user without an associated github acount" do
+        it "returns a string" do
+          expect(incomplete_profile.github).to be(nil)
+        end
+      end
+    end
+
+    describe "#github_username" do
+      context "a user with an associated github account" do
+        it "returns a string" do
+          expect(complete_profile.github_username.class).to be(String)
+        end
+      end
+
+      context "a user without an associated github acount" do
+        it "returns a string" do
           expect(incomplete_profile.github_username).to be(nil)
         end
       end
+    end
 
-      describe "#goal" do
-        context "a user who has defined themselves as a patron" do
-          it "returns nil" do
-            expect(incomplete_profile.goal).to eq(nil)
-          end
+    describe "#openstreetmap" do
+      context "a user with an associated openstreetmap account" do
+        it "returns a hash" do
+          expect(complete_profile.openstreetmap.class).to be(Hash)
         end
       end
 
-    end # an incomplete profile
-
-    context "a profile that has defined a gittip goal" do
-      before { VCR.insert_cassette "goal_profile" }
-      after { VCR.eject_cassette }
-      let(:goal_profile) { Gratitude::Profile.new("JohnKellyFerguson") }
-
-      describe "#goal" do
-        it "returns the correct goal amount" do
-          expect(goal_profile.goal).to eq(5.00)
+      context "a user without an associated openstreetmap acount" do
+        it "returns a string" do
+          expect(incomplete_profile.openstreetmap).to be(nil)
         end
       end
-    end # a profile with a gittip goal
+    end
+
+    describe "#openstreetmap_username" do
+      context "a user with an associated openstreetmap account" do
+        it "returns a string" do
+          expect(complete_profile.openstreetmap_username.class).to be(String)
+        end
+      end
+
+      context "a user without an associated openstreetmap acount" do
+        it "returns a string" do
+          expect(incomplete_profile.openstreetmap_username).to be(nil)
+        end
+      end
+    end
+
+    describe "#twitter" do
+      context "a user with an associated twitter account" do
+        it "returns a hash" do
+          expect(complete_profile.twitter.class).to be(Hash)
+        end
+      end
+
+      context "a user without an associated twitter acount" do
+        it "returns a string" do
+          expect(no_twitter_profile.twitter).to be(nil)
+        end
+      end
+    end
+
+    describe "#twitter_username" do
+      context "a user with an associated twitter account" do
+        it "returns a string" do
+          expect(complete_profile.twitter_username.class).to be(String)
+        end
+      end
+
+      context "a user without an associated twitter acount" do
+        it "returns a string" do
+          expect(no_twitter_profile.twitter_username).to be(nil)
+        end
+      end
+    end
+
+    describe "#venmo" do
+      context "a user with an associated venmo account" do
+        it "returns a hash" do
+          expect(complete_profile.venmo.class).to be(Hash)
+        end
+      end
+    end
+
+    describe "#venmo_username" do
+      context "a user with an associated venmo account" do
+        it "returns a string" do
+          expect(complete_profile.venmo_username.class).to be(String)
+        end
+      end
+
+      context "a user without an associated venmo acount" do
+        it "returns a string" do
+          expect(incomplete_profile.openstreetmap_username).to be(nil)
+        end
+      end
+    end
+
+    describe "#amount_giving" do
+      it "returns a float" do
+        expect(complete_profile.amount_giving.class).to be(Float)
+      end
+
+      it "returns the same value as its alias: #giving" do
+        expect(complete_profile.amount_giving).to eq(complete_profile.giving)
+      end
+    end
+
+    describe "#amount_receiving" do
+      it "returns a float" do
+        expect(complete_profile.amount_receiving.class).to be(Float)
+      end
+
+      it "returns the same value as its alias: #receiving" do
+        expect(complete_profile.amount_receiving)
+          .to eq(complete_profile.receiving)
+      end
+    end
+
+    describe "#goal" do
+      context "a user who has defined a goal" do
+        it "returns a Float" do
+          expect(complete_profile.goal.class).to be(Float)
+        end
+      end
+
+      context "a user who has not defined a goal" do
+        it "returns nil" do
+          expect(incomplete_profile.goal).to be(nil)
+        end
+      end
+    end
+
+    describe "#account_type" do
+      it "returns a string" do
+        expect(complete_profile.account_type.class).to be(String)
+      end
+
+      it "returns the same value as its alias: #number" do
+        expect(complete_profile.account_type).to eq(complete_profile.number)
+      end
+    end
+
+    describe "#id" do
+      it "returns a fixnum" do
+        expect(complete_profile.id.class).to be(Fixnum)
+      end
+    end
+
+    describe "#number_of_patrons" do
+      it "returns a Fixnum" do
+        expect(complete_profile.number_of_patrons.class).to be(Fixnum)
+      end
+
+      it "returns the same value as its alias: #npatrons" do
+        expect(complete_profile.number_of_patrons)
+          .to eq(complete_profile.npatrons)
+      end
+    end
+
+    describe "#on" do
+      it "returns a string" do
+        expect(complete_profile.on.class).to be(String)
+      end
+    end
 
   end # instance methods
 
